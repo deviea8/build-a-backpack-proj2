9873a17140803e0ee56c458d185fffcb16798cf8
deviea8 <deviea8@gmail.com>
2017-03-24 12:11:40 -0400

    Created post route for new backpack

--- routes/backpack_controller.js
+++ routes/backpack_controller.js
@@ -16,6 +16,34 @@ router.get('/', function(req, res) {
 });
 
 
+// new backpack (form page)
+router.get('/new', function(req, res) {
+    res.render('backpacks/new');
+});
+
+// create/post backpack
+router.post('/', function(req, res) {
+    var backpack = new Backpack({
+        backpack_name: req.body.backpack_name,
+        pencils: req.body.pencils,
+        folders: req.body.folders,
+        notebooks: req.body.notebooks,
+        scissors: req.body.scissors,
+        erasers: req.body.erasers,
+        colored_pencils: req.body.colored_pencils,
+        markers: req.body.markers,
+        glue_sticks: req.body.glue_sticks,
+        backpack_color: req.body.backpack_color,
+        recipient_note: req.body.recipient_note
+    });
+    backpack.save(function(err, backpack){
+        if (err) { console.log(err); }
+        console.log(backpack);
+        res.send(backpack);
+    });
+});
+
+
 // show individual backpack
 router.get('/:id', function(req, res) {
     Backpack.findById(req.params.id)
