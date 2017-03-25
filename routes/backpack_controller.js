7c9fac42bba86240371aa3967d828609ae1a064c
deviea8 <deviea8@gmail.com>
2017-03-24 22:28:40 -0400

    Accurately pushing new backpacks to user record

--- routes/backpack_controller.js
+++ routes/backpack_controller.js
@@ -8,28 +8,36 @@ var mongoose = require("mongoose");
 
 // New backpack (form page)
 router.get('/new', function(req, res) {
-    res.render('backpacks/new');
+    res.render('backpacks/new', {
+        userId: req.params.id
+    });
 });
 
 // Create/post backpack
-router.post('/', function(req, res) {
-    var backpack = new Backpack({
-        backpack_name: req.body.backpack_name,
-        pencils: req.body.pencils,
-        folders: req.body.folders,
-        notebooks: req.body.notebooks,
-        scissors: req.body.scissors,
-        erasers: req.body.erasers,
-        colored_pencils: req.body.colored_pencils,
-        markers: req.body.markers,
-        glue_sticks: req.body.glue_sticks,
-        backpack_color: req.body.backpack_color,
-        recipient_note: req.body.recipient_note
-    });
-    backpack.save(function(err, backpack){
-        if (err) { console.log(err); }
-        console.log(backpack);
-        res.redirect('/backpacks');
+router.post('/', function createNewBackpack(req, res) {
+    User.findById(req.params.id)
+    .exec(function(err,user){
+        if(err) {cosole.log(err)}
+        var newBackpack = new Backpack({
+            backpack_name: req.body.backpack_name,
+            pencils: req.body.pencils,
+            folders: req.body.folders,
+            notebooks: req.body.notebooks,
+            scissors: req.body.scissors,
+            erasers: req.body.erasers,
+            colored_pencils: req.body.colored_pencils,
+            markers: req.body.markers,
+            glue_sticks: req.body.glue_sticks,
+            backpack_color: req.body.backpack_color,
+            recipient_note: req.body.recipient_note
+        });
+        var userId = req.params.id;
+        user.backpacks.push(newBackpack)
+        user.save(function(err){
+            if(err) {console.log(err)};
+            console.log(newBackpack);
+        });
+        res.redirect('/users/' + userId + '/backpacks/edit');
     });
 });
 
