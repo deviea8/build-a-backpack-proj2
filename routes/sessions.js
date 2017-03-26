9d2ba6f5b93fa3d3517dbf8b077ca880af1351be
deviea8 <deviea8@gmail.com>
2017-03-26 17:20:24 -0400

    Fixed issue with routing between user and admin

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router({mergeParams: true});
 var User = require('../models/user.js');
+var Org = require('../models/org.js');
 var authHelpers = require('../helpers/auth.js')
 
 // Render login page
@@ -14,22 +15,25 @@ router.get('/login', function(req, res) {
 // Authorize & log in user
 router.post('/login', authHelpers.loginUser, function(req, res){
   console.log(req.session);
-  var orgId = req.session.currentUser.org;
-  var userId = req.session.currentUser.id;
+  var orgId = (req.session.currentUser.org).toString();
+  var userId = (req.session.currentUser.id).toString();
 
     User.findById(userId)
       .exec(function(err, user) {
-        if (user.admin === true) {
-          res.render('admin/dashboard.hbs', {
-            user: user
-          })
+        if (user.admin !== true) {
+          res.redirect('/org/' + orgId + '/users/' + userId + '/backpacks')
         }
         else {
-          res.redirect('/org/' + orgId + '/users/' + userId + '/backpacks')
+          Org.findById(orgId)
+            .exec(function(err,org){
+              res.render('admin/dashboard.hbs', {
+                org: org,
+                user: user
+              });
+          });
       };
     });
-  });
-
+});
 
 // End session
 router.delete('/', function(req, res){
