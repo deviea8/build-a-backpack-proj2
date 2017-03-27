4fbd5d7a88be865bbc94b3f597a90eb3c810f2ca
deviea8 <deviea8@gmail.com>
2017-03-27 16:43:25 -0400

    Succesfully deleted backpack

--- routes/backpack_controller.js
+++ routes/backpack_controller.js
@@ -72,34 +72,7 @@ router.get('/:backpackId/edit', function(req,res) {
     });
 });
 
-// HOW DO I DO THIS USING ORG???
-// // Update/patch backpack
-// router.patch('/:backpackId', function(req, res) {
-//     var org = req.params.orgId;
-//     var user = req.params.id;
-//     Org.findByIdAndUpdate(req.params.orgId, {
-//         backpack_name: req.body.backpack_name,
-//         pencils: req.body.pencils,
-//         folders: req.body.folders,
-//         notebooks: req.body.notebooks,
-//         scissors: req.body.scissors,
-//         erasers: req.body.erasers,
-//         colored_pencils: req.body.colored_pencils,
-//         markers: req.body.markers,
-//         glue_sticks: req.body.glue_sticks,
-//         backpack_color: req.body.backpack_color,
-//         recipient_note: req.body.recipient_note
-//     }, {new: true})
-//         .exec(function(err, backpack) {
-//             if (err) { console.log(err); }
-//             console.log(backpack);
-//             res.render('backpacks/show', {
-//               backpack: backpack,
-//               userId: req.params.id
-//             });
-//         });
-// });
-
+// Patch/update backpack after edit
 router.patch('/:backpackId', function(req,res){
     Org.findById(req.params.orgId)
         .exec(function(err,org){
@@ -130,26 +103,19 @@ router.patch('/:backpackId', function(req,res){
 });
 
 
-// HOW DO I DELETE BACKPACK FROM ORG???
-// Delete backpack - Having some issues with this - after editing a backpack the edit and delete buttons don't work anymore
-router.delete("/:backpackId", function(req, res) {
-    console.log(req.params.id)
-    console.log(req.params.orgId)
-    Org.findByIdAndUpdate(req.params.orgId, {
-        $pull: {
-            backpacks: {_id: req.params.backpackId}
-        }
-    })
-        .exec(function(err, user) {
-            if (err) { console.log(err); }
-        });
-    Backpack.findByIdAndRemove(req.params.backpackId)
-        .exec(function(err, backpack) {
-            if (err) { console.log(err); }
-            res.redirect('/org/' + req.params.orgId + '/users/' + req.params.id + '/backpacks');
-        });
+// Delete backpack
+router.delete('/:backpackId', function(req,res){
+    var userId = req.params.id;
+    Org.findById(req.params.orgId)
+    .exec(function(err,org){
+        var user = org.users.id(userId);
+        if(err) {console.log(err)}
+        org.users.id(userId).backpacks.id(req.params.backpackId).remove();
+        org.save();
+        user.save();
+        res.redirect('/org/' + req.params.orgId + '/users/' + req.params.id + '/backpacks');
+    });
 });
-
 
 
 // Show individual backpack
